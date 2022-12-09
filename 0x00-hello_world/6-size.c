a88341c5b2fc549c09c37e86385198b88ee37ca1
Mallam1234 <mallamibrahimrib19@gmail.com>
2022-12-09 00:30:21 -0800

    my task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main -print out size of data type in c
  * code by mallam
- * Return: 0
+ * Return: o
  */
 int main(void)
 {
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("size of an int: %lu byte(s))\n", (unsigned long)sizeof(b));
-	printf("size of a long int: %lubyte(s)\n", (unsigned long)sizeof(c));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
+	return (o);
 }
