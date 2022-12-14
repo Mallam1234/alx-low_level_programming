4f8354bc424795317e5bd8c85624f97eb2c5b6d9
Mallam1234 <mallamibrahimrib19@gmail.com>
2022-12-14 14:38:36 -0800

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,8 +8,15 @@
  */
 int main(void)
 {
-	int i, 'sum' = (0);
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
-
-	
-
