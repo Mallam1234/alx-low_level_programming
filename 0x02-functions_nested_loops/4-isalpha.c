efbd66e3fc5611bbcb87da6d94eb4ad4d507e211
Mallam1234 <mallamibrahimrib19@gmail.com>
2022-12-14 05:29:49 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,8 +4,8 @@
  * _isalpha - checks for char alphabet
  * @c: the char to be checked
  *
- * Return: 1 if char is letter lower or upper, 0 otherwise
-  */
+ * Return: 1 if char is letter or upper, 0 otherwise
+ */
 
 int _isalpha(int c)
 {
