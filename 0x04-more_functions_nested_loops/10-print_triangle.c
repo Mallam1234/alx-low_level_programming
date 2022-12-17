5347e707eb06bf787d31103c42e506c5f5e49c95
Mallam1234 <mallamibrahimrib19@gmail.com>
2022-12-16 16:05:56 -0800

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - prints the numbers from 1 to 100
- * 3 multiples print Fizz instead of the number
- * 5 multiples print Buzz instead of the number
- * 3 & 5  multiples print FizzBuzz instead of the number
- * Return: Always 0 (Success)
+ * print_triangle - entry point
+ * owned by Mallam/Olaoye Ibrahim
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
  */
-int main(void)
-{
-	int i;
-	char f[] = "Fizz";
-	char b[] = "Buzz";
-	char fb[] = "FizzBuzz";
 
-	for (i = 1; i <= 100; i++)
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
 	{
-		if (i == 100)
-			printf("%s", b);
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s ", fb);
-		else if (i % 3 == 0)
-			printf("%s ", f);
-		else if (i % 5 == 0)
-			printf("%s ", b);
-		else
-			printf("%d ", i);
+		_putchar('\n');
 	}
-	printf("\n");
-	return (0);
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
