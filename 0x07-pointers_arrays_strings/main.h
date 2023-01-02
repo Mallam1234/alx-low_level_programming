e3977e09443da034f8fd1ecece714c7b127c7dcd
Mallam1234 <mallamibrahimrib19@gmail.com>
2023-01-02 13:14:20 -0800

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,7 +10,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 unsigned int _strspn(char *s, char *accept);
-int _putchar(char c);
 
 
 #endif
