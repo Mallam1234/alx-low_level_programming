408b38d178b230a84592f66c44629dc228e8d282
Mallam1234 <mallamibrahimrib19@gmail.com>
2023-01-16 16:04:24 -0800

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+
+/**
+ *  * struct dog - dog class
+ *   *
+ *    * @name: Name of the dog
+ *     *
+ *      * @age: Age of the dog
+ *       *
+ *        * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
