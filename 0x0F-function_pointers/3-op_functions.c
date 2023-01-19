13c88a17a22c2270281fd3908888342653b1eeb2
Mallam1234 <mallamibrahimrib19@gmail.com>
2023-01-19 15:16:42 -0800

    task3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,29 +3,67 @@
 #include <stdio.h>
 
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
+ * op_add - ...
+ * @a: ...
+ * @b: ...
  * Return: ...
  */
-int main(int argc, char *argv[])
+int op_add(int a, int b)
 {
-	int (*oprt)(int, int);
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-	return (0);
+	return (a + b);
 }
 
+/**
+ * op_sub - ...
+ * @a: ...
+ * @b: ...
+ * Return: ...
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - ...
+ * @a: ...
+ * @b: ...
+ * Return: ...
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - ...
+ * @a: ...
+ * @b: ...
+ * Return: ...
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - ...
+ * @a: ...
+ * @b: ...
+ * Return: ...
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
